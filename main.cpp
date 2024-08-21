5d2526194c06955496691417ece4327d1c4fc758
SantiGomez2519 <sgomezo14@eafit.edu.co>
2024-08-20 19:30:56 -0500

    measure performance

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@ void setupConsole() {
 
 // para compilar el codigo: g++ book.cpp file.cpp linkedlist.cpp main.cpp node.cpp -o demo
 
-// ./demo
 
 File generator;
 File generator2;
@@ -25,12 +24,84 @@ void charge_data(File &generate, const std::string& file) {
     generate.read_from_file(file);
 }
 
-std::string simulate_full_name() {
-    std::string full_name = generator.get_random_item() + " " +
-                            generator2.get_random_item() + " " +
-                            generator2.get_random_item();
-    return full_name;
+void measure_performance_title(LinkedList& list) {
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        list.bubble_sort_title();
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "\nBubble sort by Title: " << duration << " ms" << std::endl;
+    }
+
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        list.merge_sort(BY_TITLE);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "\nMerge sort by Title: " << duration << " ms" << std::endl;
+    }
+
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        list.quick_sort_title();
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "\nQuick Sort by Title: " << duration << " ms" << std::endl;
+    }
 }
+
+void measure_performance_isbn(LinkedList& list) {
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        list.bubble_sort_isbn();
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "\nBubble sort by ISBN: " << duration << " ms" << std::endl;
+    }
+
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        list.merge_sort(BY_ISBN);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "\nMerge sort by ISBN: " << duration << " ms" << std::endl;
+    }
+
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        list.quick_sort_isbn();
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "\nQuick Sort by ISBN: " << duration << " ms" << std::endl;
+    }
+}
+
+void measure_performance_copies(LinkedList& list) {
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        list.bubble_sort_copies();
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "\nBubble sort by Copies: " << duration << " ms" << std::endl;
+    }
+
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        list.merge_sort(BY_COPIES);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "\nMerge sort by Copies: " << duration << " ms" << std::endl;
+    }
+
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        list.quick_sort_copies();
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "\nQuick Sort by Copies: " << duration << " ms" << std::endl;
+    }
+}
+
 
 int main() {
     setupConsole();
@@ -39,16 +110,13 @@ int main() {
     charge_data(generator2, "authors.txt");
 
     LinkedList list;
-    for (int i = 0; i < 10; i++) {
+
+    for (int i = 0; i < 250000; i++) {
         list.add(Book(generator.get_random_item(),generator2.get_random_item(),generator.generate_random_integer(10000000,99999999),generator.random_date(1600,2024), generator.generate_random_integer(0,500)));
     }
 
-    std::cout << "\nList before sort:" << std::endl;
-    list.print();
-
-    std::cout << "\nList after sort by title:" << std::endl;
-    list.merge_sort(BY_TITLE);
-    list.print();
+    measure_performance_title(list);
+    measure_performance_copies(list);
     
     return 0;
 }
